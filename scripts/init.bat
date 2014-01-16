1d418d62adb98e6b98059e9d143cb56c3e9d5163
Maximilian Stroh <Hisako1337@googlemail.com>
2014-01-16 22:54:25 +0100

    modified package.json

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,4 +1,5 @@
 rd /s /q node_modules
 rd /s /q bower_components
+rd /s /q .git
 npm install
 ./node_modules/.bin/bower install
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf node_modules bower_components
+rm -rf node_modules bower_components .git
 npm install
 ./node_modules/.bin/bower install
